5843aad1774ee3cb7ba7f53e3b59fc2b7124401f
aadityas201 <69757025+aadityas201@users.noreply.github.com>
2022-05-16 23:15:00 +0530

    Admin UI ✔

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import List from "./pages/list/List";
+import Add from "./pages/Add/Add";
+import { productInputs, userInputs } from "./data";
+import Single from "./pages/singlepage/Single";
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Single />
+    </div>
+  );
 }
 
 export default App;
